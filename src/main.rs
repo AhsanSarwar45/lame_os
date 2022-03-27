89e242674ac33d9fc3d240a97972aa947343b0b5
Ahsan Sarwar <ahsanrockzs@gmail.com>
2022-03-27 13:55:38 +0500

    Add custom target triple

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// PanicInfo contains the file and line no. where the panic occurs
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
