c1b5a83cca3fac26e5257cab159bdfc9e0d90cc2
Ahsan Sarwar <ahsanrockzs@gmail.com>
2022-03-30 17:50:24 +0500

    Add VGA Buffer display

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,21 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // 0xb8000 is mapped to the VGA text buffer https://en.wikipedia.org/wiki/VGA_text_mode
+    let vga_buffer : *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // |Character (8bits) |Foreground color (3 bits)|Brightness (1 bit) |Background color (3 bits)| Blink (1 bit)|
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xcb;
+        }
+    }
+    
+
     loop {}
 }
